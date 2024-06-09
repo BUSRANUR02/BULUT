8c3b1a0258bbd41ec5bba0d36bedb1e46bfc8e17
BUSRANUR02 <88855199+BUSRANUR02@users.noreply.github.com>
2024-06-09 18:00:18 +0300

    modülYazma.py

---
+++ modülYazma.py
@@ -0,0 +1,15 @@
+print("Modül eklendi.")
+number=10
+
+numbers=[1,2,3]
+
+person={
+    "name":"ali",
+     "age":"25",
+     "city":"istanbul"
+}
+def func(x):
+    print(f'x:{x}')
+    class Person:
+        def speak(self):
+            print('I am speaking...')
